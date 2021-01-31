d26d21041d685e182caa4d6d0ee267001f4fd6db
SondreB <sondre@outlook.com>
2021-02-01 00:19:35 +0100

    Add the Blockcore Data Vault UI
    - Adopted from a previously Angular UI built for Blockcore.
    - Upgrades to latest Angular.

---
+++ src/Blockcore.DataVault/ClientApp/src/app/services/applicationstate.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { EventEmitter, Injectable } from '@angular/core';
+
+@Injectable({
+   providedIn: 'root'
+})
+export class ApplicationState {
+
+   title: string;
+
+   actions: any;
+
+   constructor() {
+
+   }
+
+}
